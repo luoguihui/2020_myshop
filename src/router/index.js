0d0e7647bec9fe5a2430eb128398c38692739b75
luoguihui <Lgh5262@163.com>
2020-01-14 10:44:01 +0800

    init myshop

---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../pages/Home/Home'
+import Search from '../pages/Search/Search'
+import Order from '../pages/Order/Order'
+import Profile from '../pages/Profile/Profile'
+import Login from '../pages/Login/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/home',
+      component: Home,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
+})
